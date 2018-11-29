d3f02ce1ab22c7ee2c449680177d049ff5609a48
Yannick Kuhar <yannick.kuh@gmail.com>
2018-11-29 19:43:57 +0100

    TODO: debug prevec xov pri harris

--- exercise4/assignment1.m
+++ exercise4/assignment1.m
@@ -1,8 +1,10 @@
 % a
 
+% I = rgb2gray(imread('test_points.png'));
 I = rgb2gray(imread('graf/graf1.png'));
 I = double(I);
 
+%{
 Ih1 = hessian_points(I, 3);
 Ih2 = hessian_points(I, 6);
 Ih3 = hessian_points(I, 9);
@@ -33,19 +35,33 @@ figure(1); subplot(2, 3, 3); imagesc(Ih3); colormap gray; title('Hessian, sigma 
 figure(1); subplot(2, 3, 4); imshow(Ip1); colormap gray; 
 figure(1); subplot(2, 3, 5); imshow(Ip2); colormap gray; 
 figure(1); subplot(2, 3, 6); imshow(Ip3); colormap gray; 
-
-
-% b
-%{
-Ihar1 = I;
-
-[pxh, pyh] = harris_points(Ihar1, 1, 100);
-pos1 = [pxh(:) pyh(:)];
-
-Ihar1 = uint8(Ihar1);
 %}
 
+% b
 
+Ihar1 = I;
+Ihar2 = I;
+Ihar3 = I;
+
+[pxh1, pyh1] = harris_points(Ihar1, 3, 100);
+[pxh2, pyh2] = harris_points(Ihar2, 6, 100);
+[pxh3, pyh3] = harris_points(Ihar3, 9, 100);
+
+pos1 = [pyh1(:) pxh1(:)];
+pos2 = [pyh2(:) pxh2(:)];
+pos3 = [pyh3(:) pxh3(:)];
+
+Ihar1 = uint8(Ihar1);
+Ihar2 = uint8(Ihar2);
+Ihar3 = uint8(Ihar3);
+
+Ihar1 = insertMarker(Ihar1, pos1,'x','color','red', 'size',5);
+Ihar2 = insertMarker(Ihar2, pos2,'x','color','red', 'size',5);
+Ihar3 = insertMarker(Ihar3, pos3,'x','color','red', 'size',5);
+
+figure(1); subplot(2, 3, 4); imshow(Ihar1);
+figure(1); subplot(2, 3, 5); imshow(Ihar2);
+figure(1); subplot(2, 3, 6); imshow(Ihar3);
 
 
 
